f46b026623ee0a41b48d9651757a0fd184b5f417
kiranmayi9 <82269929+kiranmayi9@users.noreply.github.com>
2021-05-06 17:03:10 +0530

    created byte.c
    This program executes the size of int,char,float,double.

---
+++ byte.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<< "size of int: " << size of (int) <<"bytes"<<end1;
+  cout<< "size of float: " << size of (float) <<"float"<<end1;
+  cout<< "size of double: " << size of (double) <<"double"<<end1;
+  cout<< "size of char: " << size of (char) <<"char"<<end1;
+  
+  return 0;
+}
