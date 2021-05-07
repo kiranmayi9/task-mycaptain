f399d84608fd454f06b240ad16ac4c70da784b05
kiranmayi9 <82269929+kiranmayi9@users.noreply.github.com>
2021-05-07 12:41:06 +0530

    created sizess.cpp
    This program prints the number of bytes of int,float,char,double

---
+++ sizess.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    cout<<"sizeofint: " <<sizeof(int) << " bytes \n"; 
+	cout<<"sizeofchar: " <<sizeof(char) << " byte \n"; 
+    cout<<"sizeoffloat: " <<sizeof(float) << " bytes \n"; 
+	cout<<"sizeofdouble: " <<sizeof(double) << " bytes \n";
+  
+    return 0;
+ 
+}
